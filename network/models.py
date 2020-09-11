f14c10cc7f7ee81be9145fdbe1816d5413f199b0
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-09-11 11:40:46 -0700

    chore: create Post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user} on {self.timestamp}"

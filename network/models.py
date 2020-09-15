1fc442e3510dfc390d55f0a09afa2316a95c7654
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-09-14 20:30:10 -0700

    chore: create Like model

--- network/models.py
+++ network/models.py
@@ -6,17 +6,19 @@ class User(AbstractUser):
     pass
 
 class Post(models.Model):
+    """ Model reprepenting a Post. """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=500)
     timestamp = models.DateTimeField(auto_now_add=True)
-    likes = models.PositiveIntegerField(default=0)
 
     def __str__(self):
+        """ String representing a Post object. """
         return f"{self.user} on {self.timestamp}"
 
-class Following(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
-    followed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followed_by')
+class Like(models.Model):
+    """ Model representing a Like. """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    liked_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='liked_posts')
 
     def __str__(self):
-        return f"{self.user} is following {self.followed_by}"
+        return f"{self.user} likes post of {self.liked_post.user} on {self.liked_post.timestamp}"

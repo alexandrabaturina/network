fd3f3174761ebcd7523d40083a0e08b194a02c35
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-09-22 17:26:20 -0700

    chore: add url patterns

--- network/urls.py
+++ network/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("new_post", views.new_post, name="new_post")
+    path("new_post", views.new_post, name="new_post"),
+    path("following", views.following, name="following"),
+    path("profile/<str:username>", views.profile, name="profile"),
+    path("like/<int:post_id>", views.like_post, name="like"),
+    path("edit/<int:post_id>", views.edit_post, name="edit")
 ]
